eca7eb01fbe3b324afea09ef1bc5638527bd3bfa
Komron <zakhidovvkomazzz@gmail.com>
2025-06-09 06:09:28 +0500

    B

--- script.js
+++ script.js
@@ -18,16 +18,21 @@ if(close) {
 let MainImg = document.getElementById("MainImg");
 let smallImg = document.getElementsByClassName("small-img");
 
+for (let i = 0; i < smallImg.length; i++) {
+    smallImg[i].onclick = function () {
+        MainImg.src = smallImg[i].src;
+    }
+}
  
-smallImg[0].onclick = function() {
-    MainImg.src = smallImg[0].src;
-}
-smallImg[1].onclick = function() {
-    MainImg.src = smallImg[1].src;
-}     
-smallImg[2].onclick = function() {
-    MainImg.src = smallImg[2].src;
-}
-smallImg[3].onclick = function() {
-    MainImg.src = smallImg[3].src;
-}
+// smallImg[0].onclick = function() {
+//     MainImg.src = smallImg[0].src;
+// }
+// smallImg[1].onclick = function() {
+//     MainImg.src = smallImg[1].src;
+// }     
+// smallImg[2].onclick = function() {
+//     MainImg.src = smallImg[2].src;
+// }
+// smallImg[3].onclick = function() {
+//     MainImg.src = smallImg[3].src;
+// }
